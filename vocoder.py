4237d4912977f83e61fafa275eb4df8483e730b0
Oscar Quintanilla <Oscar@Oscars-iMac.local>
2021-08-29 19:28:46 -0400

    added chord voicings for later usage, added octaves, and fixed audio input and output search function

--- vocoder.py
+++ vocoder.py
@@ -1,30 +1,42 @@
 from pyo import *
 
+# Chord Voicings
+
+# CHORD = {
+#     "major7": [4, 7, 11],
+#     "minor7": [3, 7, 10],
+#     "dom7": [4, 7, 10],
+#     "halfDim7": [4, 6, 10]
+# }
+
 s = Server()
 
 # To Find what number to put in the Input and Output
-# print("Audio host APIs:")'
 
+# print("Audio host APIs:")
 # pa_list_host_apis()
-# pa_list_host_devices()
+# pa_list_devices()
 
-# print("Default input device: " % pa_get_default_input())
-# print("Default output device: " % pa_get_default_output())
+# print("Default input device: %i" % pa_get_default_input())
+# print("Default output device: %i" % pa_get_default_output())
 
-
-# Set Input & Output to Universal Audio
 
 s.setInputDevice(2)
 s.setOutputDevice(2)
-
 s.boot()
 
 mic = Input().play().out()
 
+# Octaves
+octave_high = Harmonizer(mic, transpo=12).out()
+octave_low = Harmonizer(mic, transpo=-12).out()
+
+# List of Harmonies
 harm_3 = Harmonizer(mic, transpo=4).out()
+# harm_3.ctrl(title="Harmony 3") - Visual GUI - If neeeded
+
 harm_5 = Harmonizer(mic, transpo=7).out()
 harm_7 = Harmonizer(mic, transpo=11).out()
 
-
 s.start
-s.gui(locals=())
+s.gui(locals())
